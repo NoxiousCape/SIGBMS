d89914e130090c3849f5f0c46fecf3fdf5fdb98e
Fritz1304 <freetoplay1304@gmail.com>
2023-08-22 18:48:47 -0500

    Agregado enlace simbólico al backend

---
+++ backend/src/main/java/com/example/sigbs/SigbsApplication.java
@@ -0,0 +1,13 @@
+package com.example.sigbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigbsApplication.class, args);
+	}
+
+}
