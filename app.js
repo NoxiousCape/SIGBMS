ac7f4c2653d6b502e8b49ab19bd1efafe9869297
Fritz1304 <freetoplay1304@gmail.com>
2023-08-23 00:50:38 -0500

    Actualización, valida datos

--- app.js
+++ app.js
@@ -9,3 +9,39 @@ sign_up_btn.addEventListener("click", () => {
 sign_in_btn.addEventListener("click", () => {
     container.classList.remove("sign-up-mode");
 });
+document.addEventListener('DOMContentLoaded', function() {
+    const loginForm = document.getElementById('loginForm');
+    const resultDiv = document.getElementById('result');
+
+    loginForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const username = document.getElementById('username').value;
+        const password = document.getElementById('password').value;
+
+        const credentials = {
+            username: username,
+            password: password
+        };
+
+        fetch('http://localhost:18080/Cliente/authenticate', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(credentials)
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.authenticated) {
+                resultDiv.textContent = 'Inicio de sesión exitoso';
+            } else {
+                resultDiv.textContent = 'Credenciales inválidas';
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            resultDiv.textContent = 'Error al iniciar sesión';
+        });
+    });
+});
--- backend/src/main/java/com/example/sigbs/controller/ClienteController.java
+++ backend/src/main/java/com/example/sigbs/controller/ClienteController.java
@@ -2,40 +2,36 @@ package com.example.sigbs.controller;
 
 import com.example.sigbs.entities.Cliente;
 import com.example.sigbs.services.ClienteService;
-import com.example.sigbs.util.AuthResponse;
-import com.example.sigbs.util.JwtTokenUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(origins = "*", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.OPTIONS})
 @RequestMapping("/Cliente")
 public class ClienteController {
     @Autowired
     private ClienteService clienteService;
 
-    @Autowired
-    private JwtTokenUtil jwtTokenUtil; // Asegúrate de tener esta clase configurada
 
     @PostMapping("/authenticate")
-    public ResponseEntity<?> authenticate(@RequestBody Map<String, String> request) {
-        String username = request.get("username");
-        String password = request.get("password");
+    public ResponseEntity<Map<String, Cliente>> authenticate(@RequestBody Map<String, String> credentials) {
+        String username = credentials.get("username");
+        String password = credentials.get("password");
 
-        Cliente cliente = clienteService.authenticate(username, password);
+        // Assuming you have a service method to authenticate the user
+        Cliente authenticated = clienteService.authenticate(username, password);
 
-        if (cliente != null) {
-            final String token = jwtTokenUtil.generateToken(cliente.getUsuario());
-            return ResponseEntity.ok(new AuthResponse(token));
-        } else {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Credenciales inválidas");
-        }
+        Map<String, Cliente> result = new HashMap<>();
+        result.put("authenticated", authenticated);
+
+        return ResponseEntity.ok(result);
     }
+
     @PostMapping("/saveCliente")
     public Cliente saveCliente(@RequestBody Cliente cliente){
         return clienteService.saveCliente(cliente);
